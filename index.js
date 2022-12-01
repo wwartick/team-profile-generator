832fd5070b3caf5e39cb91dfe9397b02218bfda8
wwartick <willwartick@gmail.com>
2022-12-01 15:04:57 -0800

    employee inquire

--- index.js
+++ index.js
@@ -63,6 +63,113 @@ const addManager = () => {
                     return true;
                 }
             }
+        }
+    ]) .then(managerData => {
+        const { name, id, email, officeNumber} = managerData;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        workArray.push(manager);
+    })
+};
+
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Choose a role for this employee",
+            choices: ['Engineer', 'Intern']
         },
-    ])
-}
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of this employee?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input a name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID of this employee for this project',
+            validate: idInput => {
+                if (isNaN(idInput)) {
+                    console.log('Please input an ID');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email of the employee for this project',
+            validate: emailInput => {
+                validEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (validEmailRegex) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Github of this employee?',
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please input a github');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'School of this employee?',
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please input a school');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'addMoreEmployees',
+            message: 'Are there any other employees for this project?',
+            default: 'false'
+        }
+    ]) .then(employeeData => {
+        let {name, id, email, role, github, school, addMoreEmployees} = employeeData;
+        let employee;
+
+        if (role === 'Engineer'){
+            employee = new Engineer(name, id, email, github);
+        } else if (role === 'Intern') {
+            employee = new Intern(name, id, email, school);
+        }
+
+        workArray.push(employee);
+
+        if (confirmAddEmployee) {
+            return addEmployee(workArray);
+        } else {
+            return workArray;
+        }
+    })
+};
